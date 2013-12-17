7ea28df4fda7b0435e1adbf3329cbc48a26a05b4
Noah D. Brenowitz <nbren12@gmail.com>
2013-12-17 00:47:53 -0500

    added docstring to pgram

--- python/madrespek.py
+++ python/madrespek.py
@@ -1,7 +1,14 @@
+import matplotlib.pyplot as plt
+
+
 def pgram(x, fs=1.0):
-    from scipy import signal
-    from scipy import fft
+    """
+    (C) Noah D. Brenowitz
+    
+    A function to plot the periodogram of a signal x
+    """
+    from numpy import fft
     ps  = abs(fft.fft(x**2))**2
     fs  = fft.fftfreq(x.shape[0],fs)
 
-    return semilogx(ps,fs)
+    return plt.semilogx(fs, ps)
