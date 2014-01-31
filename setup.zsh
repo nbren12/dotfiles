ac1d97e94acec7304800c2abb3baaaba9a5d054e
Noah D. Brenowitz <nbren12@gmail.com>
2014-01-31 00:51:18 -0500

    added to setup

--- setup.zsh
+++ setup.zsh
@@ -1,8 +1,12 @@
 #!/bin/zsh
 
 foreach file (emacs vimrc Rprofile gitconfig vimrc vim emacs.d 
-   zsh/zshrc zsh/cims.zsh )
+   zsh/zshrc zsh/cims.zsh unison )
 {
-    ln -s ~/.dotfiles/$file ~/.${file:t}
+    DEST=~/.${file:t}
+    if [[ ! -e  $DEST ]]
+    then
+        ln -s ~/.dotfiles/$file 
+    fi
 
 }
