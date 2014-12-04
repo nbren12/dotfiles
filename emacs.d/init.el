88dbde3848a80e7f9ef0000f25f32a5270a9f589
Noah D. Brenowitz <nbren12@gmail.com>
2014-12-04 02:12:51 -0500

    changes

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -106,17 +106,3 @@
 (setq deft-text-mode 'org-mode)
 (setq deft-directory "~/Dropbox/Notes")
 (setq deft-use-filename-as-title t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(inhibit-startup-screen t)
- '(org-agenda-files (quote ("~/Dropbox/Notes/Climate Dynamics Rejection.org"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
