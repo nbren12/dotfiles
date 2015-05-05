7c19176fab8b0ef9a2c40f66b8f5f08d9bf153cc
Noah D. Brenowitz <nbren12@gmail.com>
2015-05-04 21:02:45 -0400

    adding init.el for setting.org tangling

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,23 +1,23 @@
-;;; init.el --- My init.el
-;;; Commentary:
-;;
-;;; Code:
+  ;;; init.el --- My init.el
+  ;;; Commentary:
+  ;;
+  ;;; Code:
 
-(require 'org)
-(org-babel-load-file
- (expand-file-name "settings.org"
-                   user-emacs-directory))
-
-
+  (require 'org)
+  (org-babel-load-file
+   (expand-file-name "settings.org"
+                     user-emacs-directory))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
  '(custom-enabled-themes (quote (monokai)))
  '(custom-safe-themes
    (quote
-    ("a041a61c0387c57bb65150f002862ebcfe41135a3e3425268de24200b82d6ec9" default))))
+    ("a041a61c0387c57bb65150f002862ebcfe41135a3e3425268de24200b82d6ec9" "4dd1b115bc46c0f998e4526a3b546985ebd35685de09bc4c84297971c822750e" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
