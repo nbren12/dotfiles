6b8bdd088f24a5daa39cdd8c52061f0eee26f0ed
Noah D. Brenowitz <nbren12@gmail.com>
2015-05-12 09:52:37 -0400

    installed irony mode and fixed outshine completion

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -219,7 +219,37 @@
   (lambda() 
     (local-set-key  (kbd "C-c o") 'ff-find-other-file)))
 
+(use-package  irony
+  :ensure t
+  :config
+  (progn
+    (add-hook 'c++-mode-hook 'irony-mode)
+    (add-hook 'c-mode-hook 'irony-mode)
+    (add-hook 'objc-mode-hook 'irony-mode)
 
+    ;; replace the `completion-at-point' and `complete-symbol' bindings in
+    ;; irony-mode's buffers by irony-mode's function
+    (defun my-irony-mode-hook ()
+      (define-key irony-mode-map [remap completion-at-point]
+	'irony-completion-at-point-async)
+      (define-key irony-mode-map [remap complete-symbol]
+	'irony-completion-at-point-async))
+    (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+    (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+    ))
+
+(use-package company-irony
+  :ensure t
+  :config
+  (progn
+    (eval-after-load 'company
+      '(add-to-list 'company-backends 'company-irony))
+
+    ;; (optional) adds CC special commands to `company-begin-commands' in order to
+    ;; trigger completion at interesting places, such as after scope operator
+    ;;     std::|
+    (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+    ))
 ;;;; LaTeX
 
 (defun config/latex  ()
@@ -298,7 +328,11 @@
   (progn 
     (require 'outshine)
     (add-hook 'outline-minor-mode-hook 'outshine-hook-function)
-    (add-hook 'emacs-lisp-mode-hook 'outline-minor-mode)))
+    (add-hook 'emacs-lisp-mode-hook 'outline-minor-mode)
+
+    ; Fix company completion in outshine buffers
+    (add-to-list 'company-begin-commands 'outshine-self-insert-command)
+    ))
 
 ;;;; Org
 (setq org-use-speed-commands t)
