42a67b574b0365cf7b82b8dad9c7ef60d274522e
Noah D. Brenowitz <nbren12@gmail.com>
2015-05-18 14:41:57 -0400

    map gj and gk to j and k

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -78,7 +78,13 @@
             (evil-emacs-state-modes (cons 'org-mode evil-emacs-state-modes)))
         ad-do-it
         (evil-change-state orig-state)))
-
+;; Make movement keys work like they should
+(define-key evil-normal-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
+(define-key evil-normal-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
+(define-key evil-motion-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
+(define-key evil-motion-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
+; Make horizontal movement cross lines                                    
+(setq-default evil-cross-lines t)
     ))
 
 (use-package evil-leader
