e4ab966048e8e267ffc56e3eca2362991c96115c
Noah D. Brenowitz <nbren12@gmail.com>
2015-07-14 10:51:52 -0400

    added lein projil

---
+++ .lein/profiles.clj
@@ -0,0 +1,6 @@
+{:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [refactor-nrepl "0.3.0-SNAPSHOT"]
+                  [incanter "1.2.3-SNAPSHOT"]
+                  [net.mikera/vectorz-clj "0.30.1"]]
+         :dependencies [[alembic "0.3.2"]
+                        [org.clojure/tools.nrepl "0.2.7"]]}}
