18f6e95805641a6a791b4416d92d3451b28643ce
Noah D. Brenowitz <nbren12@gmail.com>
2016-03-06 00:20:23 -0500

    create util.zsh move bash to shell

---
+++ shell/util.zsh
@@ -0,0 +1,29 @@
+function fd ()
+{
+    cd $(find . -maxdepth 3 -not -path '*/\.*' -type d | fzf )
+}
+
+function projectroot ()
+{
+    markers=(.git .top)
+    d=`pwd`
+    gdir="/"
+
+    while [[ ! $d == "/" ]]
+    do
+        for marker in $markers; do
+            if [[ -e $d/$marker ]]; then
+                gdir=$d
+            fi
+        done
+        d=$(dirname $d)
+    done
+    echo $gdir
+}
+
+function findignore()
+{
+    find $1 -not -path '*\.git/*'
+}
+
+alias lp='findignore `projectroot` | fzf'
