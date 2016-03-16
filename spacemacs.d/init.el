e944f1433a52107532580d188aed28e13bcfa784
Noah D. Brenowitz <nbren12@gmail.com>
2016-03-15 23:49:02 -0700

    remove private/snippets from yasnippet dirs

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -416,3 +416,18 @@ layers configuration."
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(yas-snippet-dirs
+   (quote
+    ("/Users/noah/.spacemacs.d/snippets" yas-installed-snippets-dir "/Users/noah/.emacs.d/layers/auto-completion/snippets" "/Users/noah/.emacs.d/elpa/clojure-snippets-20160305.602/snippets")) nil (yasnippet)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
+ '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil)))))
