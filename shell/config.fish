2a02560675e84d6522fa45cbb32d26423273e002
Noah D. Brenowitz <nbren12@gmail.com>
2016-06-23 12:48:56 -0400

    add anaconda to path

--- shell/config.fish
+++ shell/config.fish
@@ -3,3 +3,5 @@ alias vim nvim
 alias ed 'emacs --daemon'
 alias ec 'emacsclient'
 alias ecc 'emacsclient -c'
+
+set -gx PATH /Users/noah/anaconda/bin /usr/local/texlive/2014/bin/universal-darwin $PATH
--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -60,6 +60,7 @@ values."
      vimish-fold
      multiple-cursors
      shell
+     bibtex
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
@@ -456,11 +457,12 @@ layers configuration."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(evil-want-Y-yank-to-eol nil)
  '(org-agenda-files
    (quote
-    ("~/Dropbox/notes/Admin.org" "~/Dropbox/notes/EAC/EAC.org" "~/Dropbox/notes/CRM.org")))
+    ("~/Dropbox/journal/2016-06-09/questions.org" "~/Dropbox/notes/Admin.org" "~/Dropbox/notes/EAC/EAC.org" "~/Dropbox/notes/CRM.org")))
  '(org-directory "~/Dropbox/notes")
- '(org-ref-pdf-directory "~/Dropbox/Papers/bibtex-pdfs/" t)
+ '(org-ref-pdf-directory "~/Dropbox/Papers/bibtex-pdfs/")
  '(word-wrap t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
