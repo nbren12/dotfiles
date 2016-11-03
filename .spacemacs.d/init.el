e67e38c192b46c2a8d37f2d9d362a1026279ba8d
Noah D. Brenowitz <nbren12@gmail.com>
2016-11-03 18:22:00 -0400

    add misterioso theme

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -132,6 +132,7 @@ values."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(spacemacs-dark
+                         misterioso
                          spacemacs-light
                          solarized-dark
                          solarized-light
