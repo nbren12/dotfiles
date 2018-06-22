5483b744c631077173becdaffc66c27db820af40
Noah D. Brenowitz <nbren12@gmail.com>
2018-06-21 22:50:01 -0700

    fix docstring handling

---
+++ .spacemacs.d/config/python.el
@@ -0,0 +1,7 @@
+;; Fix indentation in python docstrings.
+;; https://emacs.stackexchange.com/questions/26435/how-can-i-disable-indentation-rules-within-docstrings-in-python-mode
+(defun my-python-noindent-docstring (&optional _previous)
+  (if (eq (car (python-indent-context)) :inside-docstring)
+      'noindent))
+
+(advice-add 'python-indent-line :before-until #'my-python-noindent-docstring)
