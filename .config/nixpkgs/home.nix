8b3f04fe8e03ea2170a2dc46258a9edd3528e190
Noah D. Brenowitz <nbren12@gmail.com>
2021-01-06 09:58:39 -0800

    Add "z" for directory switching

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -85,6 +85,16 @@
       bind -M insert \cg 'git status'
     '';
 
+    plugins = [{
+      name = "z";
+      src = pkgs.fetchFromGitHub {
+        owner = "jethrokuan";
+        repo = "z";
+        rev = "ddeb28a7b6a1f0ec6dae40c636e5ca4908ad160a";
+        sha256 = "0c5i7sdrsp0q3vbziqzdyqn4fmp235ax4mn4zslrswvn8g3fvdyh";
+      };
+    }];
+
   };
   home.file.".config/fish/conf.d/prompt.fish".source =
     ~/dotfiles/.config/fish/conf.d/prompt.fish;
